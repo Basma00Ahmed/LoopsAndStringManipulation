888a85fb87d2cb095d470f46ed6ae21e3d5eefab
Basma Ahmed <basma00ahmed@outlook.com>
2021-06-16 12:01:28 +0200

    Add project files.

---
+++ LoopsAndStringManipulation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoopsAndStringManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
